a5251dbaf26aa5e49180dc4cbe5d00d506d4cc15
Alejandro <68039905+jarcasta@users.noreply.github.com>
2023-07-12 10:53:49 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/DevelSystems/SmartID-iOS", from: "4.5.0_30"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
