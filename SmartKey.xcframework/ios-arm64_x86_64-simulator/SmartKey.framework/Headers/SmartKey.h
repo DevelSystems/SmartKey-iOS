2e207183891324aabead083dfee5ce1bfbdf2bc2
Alejandro <68039905+jarcasta@users.noreply.github.com>
2023-07-12 10:49:04 -0600

    Added xcframework

---
+++ SmartKey.xcframework/ios-arm64_x86_64-simulator/SmartKey.framework/Headers/SmartKey.h
@@ -0,0 +1,18 @@
+//
+//  SmartKey.h
+//  SmartKey
+//
+//  Created by DevOps on 8/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmartKey.
+FOUNDATION_EXPORT double SmartKeyVersionNumber;
+
+//! Project version string for SmartKey.
+FOUNDATION_EXPORT const unsigned char SmartKeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartKey/PublicHeader.h>
+
+
